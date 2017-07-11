a409e065297a9a588dd12439b96aa356500497e8
elless2005 <elless2005@gmail.com>
2017-07-11 06:22:37 -0400

    sql file updated

--- tournament.sql
+++ tournament.sql
@@ -9,8 +9,26 @@
 \c tournament
 drop table if exists players cascade;
 drop table if exists matches cascade;
+drop view if exists wins cascade;
+drop view if exists games cascade;
 drop view if exists standings cascade;
 create table players (player_id serial primary key, name text);
 create table matches (match_no serial primary key, winner integer references players(player_id), loser integer references players(player_id));
-create view standings as select players.player_id, players.name, count (matches.winner) as wins, count (matches.match_no) as games from players 
-    left join matches on players.player_id = matches.winner or players.player_id = matches.loser group by players.player_id order by wins desc;
+
+create view wins as select players.player_id, players.name, count (matches.winner) as wins
+    from players left join matches
+    on players.player_id = matches.winner
+    group by players.player_id;
+
+create view games as select players.player_id, players.name, count (matches.loser + matches.winner) as total_games
+    from players left join matches
+    on players.player_id = matches.winner or players.player_id = matches.loser
+    group by players.player_id;
+
+create view standings as select players.player_id, players.name, wins.wins, games.total_games
+    from players left join wins
+    on players.player_id = wins.player_id
+    left join games
+    on players.player_id = games.player_id
+    order by wins desc;
+
